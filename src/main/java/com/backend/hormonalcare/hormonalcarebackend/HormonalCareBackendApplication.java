320ef3c65901a5ad08458a3b1be1ab0c0aa8fddc
Sandro <129917104+ClaudioSandro@users.noreply.github.com>
2024-08-30 02:23:51 -0500

    chore: first commit

---
+++ src/main/java/com/backend/hormonalcare/hormonalcarebackend/HormonalCareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.hormonalcare.hormonalcarebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HormonalCareBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HormonalCareBackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/backend/hormonalcare/hormonalcarebackend/shared/resources/MessageResource.java
@@ -0,0 +1,4 @@
+package com.backend.hormonalcare.hormonalcarebackend.shared.resources;
+
+public record MessageResource(String message) {
+}
