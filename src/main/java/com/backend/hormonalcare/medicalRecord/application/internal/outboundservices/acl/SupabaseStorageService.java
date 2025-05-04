4a736ac7094c7ee4b898a696f4568df9a42b1241
Sandro <129917104+ClaudioSandro@users.noreply.github.com>
2025-05-04 15:14:36 -0500

    feat: reduce image size with Thumbnails dependency

--- src/main/java/com/backend/hormonalcare/medicalRecord/application/internal/outboundservices/acl/SupabaseStorageService.java
+++ src/main/java/com/backend/hormonalcare/medicalRecord/application/internal/outboundservices/acl/SupabaseStorageService.java
@@ -3,7 +3,10 @@ package com.backend.hormonalcare.medicalRecord.application.internal.outboundserv
 import okhttp3.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import net.coobird.thumbnailator.Thumbnails;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -21,19 +24,25 @@ public class SupabaseStorageService {
     public String uploadFile(byte[] fileData, String originalFileName) throws IOException {
         String uniqueFileName = UUID.randomUUID() + "-" + originalFileName.replaceAll("[^a-zA-Z0-9\\.\\-]", "_");
 
-        // Determinar el Content-Type basado en la extensión del archivo
-        String contentType = Files.probeContentType(Paths.get(originalFileName));
-        if (contentType == null) {
-            contentType = "application/octet-stream"; // Valor predeterminado
-        }
+        // Redimensionar la imagen a 64x64 píxeles
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(fileData);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        Thumbnails.of(inputStream)
+                  .size(64, 64)
+                  .outputFormat("jpg") // Convertir a JPG para mayor compresión
+                  .toOutputStream(outputStream);
 
-        RequestBody requestBody = RequestBody.create(fileData, MediaType.parse(contentType));
+        byte[] resizedImageData = outputStream.toByteArray();
+
+        // Determinar el Content-Type basado en la extensión del archivo
+        String contentType = "image/jpeg"; // Forzar el tipo de contenido como imagen JPEG
+
+        RequestBody requestBody = RequestBody.create(resizedImageData, MediaType.parse(contentType));
 
         Request request = new Request.Builder()
                 .url(properties.getUrl() + "/storage/v1/object/" + properties.getBucket() + "/" + uniqueFileName)
                 .addHeader("apikey", properties.getKey())
                 .addHeader("Authorization", "Bearer " + properties.getKey())
-                .addHeader("Content-Type", contentType)
                 .put(requestBody)
                 .build();
 
