0aca04b7a19d89ef7f9048881840a025373da570
Claudio Sandro Quispesivana Torres <u202215099@upc.edu.pe>
2025-06-20 16:27:51 -0500

    refactor: infrastructur refactored

--- src/main/java/com/backend/hormonalcare/medicalRecord/infrastructure/persistence/jpa/repositories/MedicationTypeRepository.java
+++ src/main/java/com/backend/hormonalcare/medicalRecord/infrastructure/persistence/jpa/repositories/MedicationTypeRepository.java
@@ -1,6 +1,5 @@
 package com.backend.hormonalcare.medicalRecord.infrastructure.persistence.jpa.repositories;
 
-import com.backend.hormonalcare.medicalRecord.domain.model.aggregates.Medication;
 import com.backend.hormonalcare.medicalRecord.domain.model.entities.MedicationType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- src/main/java/com/backend/hormonalcare/medicalRecord/infrastructure/persistence/jpa/repositories/PrescriptionRepository.java
+++ src/main/java/com/backend/hormonalcare/medicalRecord/infrastructure/persistence/jpa/repositories/PrescriptionRepository.java
@@ -1,6 +1,5 @@
 package com.backend.hormonalcare.medicalRecord.infrastructure.persistence.jpa.repositories;
 
-import com.backend.hormonalcare.medicalRecord.domain.model.aggregates.MedicalRecord;
 import com.backend.hormonalcare.medicalRecord.domain.model.entities.Prescription;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
